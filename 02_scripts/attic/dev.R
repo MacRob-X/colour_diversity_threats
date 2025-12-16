77887b3e68e496e27eb1e7e221702597044fd4d0
MacRob-X <robert-macdonald@live.co.uk>
2025-12-16 17:16:53 +0000

    Get threat scope and severity

--- 02_scripts/attic/dev.R
+++ 02_scripts/attic/dev.R
@@ -4,43 +4,9 @@
 # Obtain threat info from IUCN redlist
 # Bastardised/updated code from Stewart et al 2025
 
-# set iucn API key
-api <- "zNXM3miBBknHAGv5mKZraLuhPg2sUTs5c2Ay"
+# Functions ----
 
-# get full list of birds in IUCN data
-#iucn_aves = rredlist::rl_class(class = "aves", key = api)
-
-# save, if necessary
-# saveRDS(
-#   iucn_aves,
-#   file = here::here(
-#     "3_output_data", "iucn_data_class_aves.RDS"
-#     
-#   )
-# )
-
-# read in again, if necessary
-iucn_aves <- readRDS(
-  here::here(
-  "3_output_data", "iucn_data_class_aves.RDS"
-  )
-)
-
-# get data
-aves_data <- iucn_aves$assessments
-
-# get unique species names with underscore
-iucn_species <- unique(gsub(" ", "_", aves_data$taxon_scientific_name))
-# keep only full species
-iucn_species <- iucn_species[which(stringr::str_count(iucn_species, "_") == 1)]
-
-# get IUCN threat classification scheme
-iucn_threat_types <- rredlist::rl_threats(key = api)$threats
-# restrict to second-level classification (after Stewart et al 2025)
-second_ord_threats <- stringr::str_extract(iucn_threat_types$code, "[^_]*_[^_]*")
-second_ord_threats <- unique(second_ord_threats[!is.na(second_ord_threats)])
-
-# function to create or append CSV of threat data
+# create or append CSV of threat data
 handle_csv <- function(iucn_species, binomial_name, csv_writepath, threats_df){
   
   # if first species in list, initialise CSV to save into
@@ -60,6 +26,7 @@ handle_csv <- function(iucn_species, binomial_name, csv_writepath, threats_df){
     }
     
     # initialise csv
+    message("Creating CSV.")
     write.csv(threats_df, file = csv_writepath)
     
   } else {
@@ -79,7 +46,7 @@ handle_csv <- function(iucn_species, binomial_name, csv_writepath, threats_df){
 # Probably the easiest way is to use the latest IUCN Red List data and rerun Chapter 1 global 
 # diversity loss analysis with the newest IUCN version - this should show that the results are 
 # near-identical and it's fine to use the newest version of the threats as well
-get_threat_data <- function(binomial_name, latest = TRUE, year_cutoff = NULL, csv_writepath = NULL){
+get_threat_data <- function(binomial_name, threats_skeleton, latest = TRUE, year_cutoff = NULL, csv_writepath = NULL){
   
   # print species processing
   cat("\r", paste("Processing ", which(iucn_species %in% binomial_name), " of ", length(iucn_species), ": ",  binomial_name, sep = ""))
@@ -110,11 +77,13 @@ get_threat_data <- function(binomial_name, latest = TRUE, year_cutoff = NULL, cs
         assess_id <- species_history[which(species_history$year_published <= year_cutoff)[1], "assessment_id"]
         species_data <- rredlist::rl_assessment(id = assess_id, key = api)
       } else {
-        # if there isn't, return NAs
-        message("No IUCN data pre-cutoff year for ", binomial_name, ". Returning row of NAs.")
-        threats_df <-  matrix(NA, nrow = 1, ncol = length(second_ord_threats) + 3)
-        colnames(threats_df) <- c("species", "iucn_cat", "assessment_year", second_ord_threats)
-        threats_df[, "species"] <- binomial_name
+        # if there isn't, use the skeleton but populated with "no_precutoff_data"
+        warning("No IUCN data pre-cutoff year for ", binomial_name, ". Returning row of 'no_precutoff_data'.")
+        threat_data <- threats_skeleton
+        threat_data[1, ] <- "no_precutoff_data"
+        assessment_year <- "no_precutoff_data"
+        iucn_cat <- "no_precutoff_data"
+        threats_df <- cbind(binomial_name, threat_data, assessment_year, iucn_cat)
         # save CSV if required
         if(!is.null(csv_writepath)){
           handle_csv(iucn_species = iucn_species, binomial_name = binomial_name, csv_writepath = csv_writepath, threats_df = threats_df)
@@ -129,31 +98,41 @@ get_threat_data <- function(binomial_name, latest = TRUE, year_cutoff = NULL, cs
   
   
   # extract threat data
-  threat_data <- species_data$threats$code
-  # if species is LC this will return NULL - this will then populate the threat matrix with 0
+  threat_data <- species_data$threats
+  # if there are no threats is LC then we use the threats skeleton - this will then populate the threat matrix with NA
   # for this species
+  if(length(threat_data) == 0){
+    threat_data <- threats_skeleton
+  }
+  # remove "description" column as we don't need it and it contains commas, which messes with
+  # CSV writing
+  threat_data$description <- NULL
   
   # restrict to second-level threat classification (after Stewart et al 2025)
-  threat_data <- stringr::str_extract(threat_data, "[^_]*_[^_]*") # keep first two levels and remove second underscore and everything after
+  #threat_data <- stringr::str_extract(threat_data, "[^_]*_[^_]*") # keep first two levels and remove second underscore and everything after
   # keep only unique second-level threat classifications
-  threat_data <- unique(threat_data)
+  #threat_data <- unique(threat_data)
   
   # initialise df for data (following IUCN threat classification scheme)
   # https://www.iucnredlist.org/resources/threat-classification-scheme
-  threats_df <- matrix(NA, nrow = 1, ncol = length(second_ord_threats) + 3)
-  colnames(threats_df) <- c("species", "iucn_cat", "assessment_year", second_ord_threats)
-  threats_df[, "species"] <- binomial_name
-  threats_df[, "iucn_cat"] <- species_data$red_list_category$code
-  threats_df[, "assessment_year"] <- species_data$year_published
+  assessment_year <- species_data$year_published
+  iucn_cat <- species_data$red_list_category$code
+  threats_df <- cbind(binomial_name, threat_data, assessment_year, iucn_cat)
+  # for second-order threats only
+  # threats_df <- matrix(NA, nrow = 1, ncol = length(iucn_threat_types) + 3)
+  # colnames(threats_df) <- c("species", "iucn_cat", "assessment_year", iucn_threat_types)
+  # threats_df[, "species"] <- binomial_name
+  # threats_df[, "iucn_cat"] <- species_data$red_list_category$code
+  # threats_df[, "assessment_year"] <- species_data$year_published
   
-  threats_df[, second_ord_threats] <- unlist(lapply(colnames(threats_df), function(col){
-    if(!(col %in% c("species", "iucn_cat", "assessment_year")) & col %in% threat_data){
-      threats_df[, col] <- 1
-    } else if(!(col %in% c("species", "iucn_cat", "assessment_year")) & !(col %in% threat_data)){
-      threats_df[, col] <- 0
-    } 
-    
-  }))
+  # threats_df[, iucn_threat_types] <- unlist(lapply(colnames(threats_df), function(col){
+  #   if(!(col %in% c("species", "iucn_cat", "assessment_year")) & col %in% threat_data){
+  #     threats_df[, col] <- 1
+  #   } else if(!(col %in% c("species", "iucn_cat", "assessment_year")) & !(col %in% threat_data)){
+  #     threats_df[, col] <- 0
+  #   } 
+  #   
+  # }))
   
   # 2 second delay makes API work better - recommended by IUCN
   Sys.sleep(2)
@@ -170,38 +149,72 @@ get_threat_data <- function(binomial_name, latest = TRUE, year_cutoff = NULL, cs
   
 }
 
-debug(get_threat_data)
+# Workflow ----
+
+# set iucn API key
+api <- "zNXM3miBBknHAGv5mKZraLuhPg2sUTs5c2Ay"
+
+# get full list of birds in IUCN data
+#iucn_aves = rredlist::rl_class(class = "aves", key = api)
+
+# save, if necessary
+# saveRDS(
+#   iucn_aves,
+#   file = here::here(
+#     "3_output_data", "iucn_data_class_aves.RDS"
+#     
+#   )
+# )
+
+# read in again, if necessary
+iucn_aves <- readRDS(
+  here::here(
+  "03_output_data", "iucn_data_class_aves.RDS"
+  )
+)
+
+# get data
+aves_data <- iucn_aves$assessments
+
+# get unique species names with underscore
+iucn_species <- unique(gsub(" ", "_", aves_data$taxon_scientific_name))
+# keep only full species
+iucn_species <- iucn_species[which(stringr::str_count(iucn_species, "_") == 1)]
+
+# get IUCN threat classification scheme
+iucn_threat_types <- rredlist::rl_threats(key = api)$threats
+# restrict to second-level classification (after Stewart et al 2025)
+# second_ord_threats <- stringr::str_extract(iucn_threat_types$code, "[^_]*_[^_]*")
+# second_ord_threats <- unique(second_ord_threats[!is.na(second_ord_threats)])
+
+
+# debug(get_threat_data)
+
+# set up threat skeleton - we will use this for species which have no threats
+# use a species which has threats, but set them to NA
+# let's use the assessment ID for the 2023 assessment of Aburria aburri
+am_assess <- rredlist::rl_species(genus = "Aburria", species = "aburri", key = api)$assessments
+a_a_assess_id <- am_assess[am_assess$year_published == "2023", "assessment_id"]
+threats_skeleton <- rredlist::rl_assessment(id = a_a_assess_id, key = api)$threats[1, ]
+threats_skeleton[1, ]="NA"
 
 # get threat data for all species - with a 2023 cutoff
 cutoff_year <- 2023
 filename <- paste("iucn_threat_matrix", cutoff_year, "cutoff_year.csv", sep = "_")
 writepath <- here::here(
-  "3_output_data", filename
+  "03_output_data", filename
 )
-allspec_threat_data <- lapply(iucn_species[608:1000], get_threat_data, latest = FALSE, year_cutoff = cutoff_year, csv_writepath = writepath)
+allspec_threat_data <- lapply(iucn_species[101:750], get_threat_data, latest = FALSE, year_cutoff = cutoff_year, csv_writepath = writepath, threats_skeleton = threats_skeleton)
 threat_matrix <- do.call(rbind, allspec_threat_data)
 
-# save as CSV
-filename <- paste("iucn_threat_matrix", cutoff_year, "cutoff_year.csv", sep = "_")
-write.csv(
-  threat_matrix,
-  here::here(
-    "3_output_data", filename
-  )
-)
 
 # get threat data for all species - latest data
-allspec_threat_data <- lapply(iucn_species, get_threat_data, latest = TRUE)
-threat_matrix <- do.call(rbind, allspec_threat_data)
-
-# save as CSV
-filename <- paste0("iucn_threat_matrix_latest_", Sys.Date(), ".csv")
-write.csv(
-  threat_matrix,
-  here::here(
-    "3_output_data", filename
-  )
+filename <- paste("iucn_threat_matrix_latest_", Sys.Date(), ".csv")
+writepath <- here::here(
+  "03_output_data", filename
 )
+allspec_threat_data <- lapply(iucn_species, get_threat_data, latest = TRUE, csv_writepath = writepath, threats_skeleton = threats_skeleton)
+threat_matrix <- do.call(rbind, allspec_threat_data)
 
 
 
@@ -209,9 +222,140 @@ write.csv(
 # Match taxonomies ----
 
 
+# Regress distance to centroid against threat type ----
+
+library(dplyr)
+library(ggplot2)
+
+# load threat matrix
+cutoff_year <- 2023
+filename <- paste("iucn_threat_matrix", cutoff_year, "cutoff_year.csv", sep = "_")
+threat_matrix <- read.csv(
+  here::here(
+    "03_output_data", filename
+  )
+)
+
+# load colour pattern space (created in Chapter 1 - patch-pipeline)
+colspace_path <- "G:/My Drive/patch-pipeline/2_Patches/3_OutputData/Neognaths/2_PCA_ColourPattern_spaces/1_Raw_PCA/Neognaths.matchedsex.patches.250716.PCAcolspaces.rds"
+colour_space <- readRDS(colspace_path)[["lab"]][["x"]]
+
+# add second-order threat codes
+threat_matrix$second_ord_code <- stringr::str_extract(threat_matrix$code, "[^_]*_[^_]*")
+
+# assign second-order IUCN threat types to grouped 'driver of extinction' categories
+# same system as Stewart et al 2025 Nat Ecol Evol - grouping is provided in Supplementary Dataset 1
+# of that paper
+# There are many threats that aren't assigned to one of these groups - this is because these
+# threats were non-significant in predicting IUCN threat level in Stewart et al 2025
+
+# Groups
+# Accidental mortality and disturbance
+acc_mort_codes <- c("4_2", "5_4", "6_3")
+# Climate change and severe weather
+clim_chan_codes <- c("11_1", "11_4")
+# Habitat loss and degradation
+hab_loss_codes <- c("1_2", "1_3", "2_1", "2_2", "2_3", "5_3", "7_1", "7_2")
+# Hunting and collecting
+hunt_col_codes <- "5_1"
+# Invasive species and disease
+invas_spec_codes <- c("8_1", "8_2")
+# Other ["Threats that affected ten or fewer species were grouped with other threats"]
+other_codes <- c("10_1", "10_2", "10_3", "12_1")
+# Pollution
+pollut_codes <- "9_3"
+
+threat_matrix <- threat_matrix |> 
+  mutate(
+    ex_driver = second_ord_code
+  ) |> 
+  mutate( # surely there's a more elegant way to do this
+    ex_driver = ifelse(
+      ex_driver %in% acc_mort_codes,
+      "acc_mort",
+      ifelse(
+        ex_driver %in% clim_chan_codes,
+        "clim_chan",
+        ifelse(
+          ex_driver %in% hab_loss_codes,
+          "hab_loss",
+          ifelse(
+            ex_driver %in% hunt_col_codes,
+            "hunt_col",
+            ifelse(
+              ex_driver %in% invas_spec_codes,
+              "invas_spec",
+              ifelse(
+                ex_driver %in% other_codes,
+                "other",
+                ifelse(
+                  ex_driver %in% pollut_codes,
+                  "pollut",
+                  ifelse(
+                    is.na(ex_driver),
+                    NA,
+                    "FLAG"
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+
+# Plot 
+threat_matrix |>
+  ggplot(aes(x = ex_driver)) + 
+  geom_bar()
+
+
+# calculate distance to centroid from colourspace
+centr_dists <- dispRity::dispRity(colour_space, metric = dispRity::centroids)$disparity[[1]][[1]]
+centr_dists <- data.frame(
+  species = sapply(strsplit(rownames(colour_space), split = "-"), "[", 1),
+  sex = sapply(strsplit(rownames(colour_space), split = "-"), "[", 2),
+  centr_dists = centr_dists
+  )
+
+# add distance to centroid onto threat matrix
+threat_centr <- threat_matrix |> 
+  full_join(centr_dists, by = join_by("binomial_name" == "species"))
+
+# remove duplicates based on second-order code
+threat_centr_clean <- threat_centr |> 
+  distinct(second_ord_code, binomial_name, sex, .keep_all = TRUE)
+
+# boxplot of centroid distances by threat type
+threat_centr_clean |> 
+  filter(
+    !is.na(sex)
+  ) |> 
+  ggplot(aes(x = ex_driver, y = centr_dists, fill = ex_driver)) + 
+  geom_boxplot() + 
+  facet_grid(~ sex)
+
+# ANOVA to check if mean centroid distances of each extinction driver are different
+an_m <- aov(centr_dists ~ ex_driver, data = threat_centr_clean[threat_centr_clean$sex == "M", ])
+summary(an_m)
+# post-hoc Tukey test
+lm_m <- lm(centr_dists ~ ex_driver, data = threat_centr_clean[threat_centr_clean$sex == "M", ])
+av_m <- aov(lm_m)
+TukeyHSD(av_m)
+
+log_mod_f <- lm(centr_dists ~ ex_driver - 1, data = threat_centr_clean[threat_centr_clean$sex == "F", ])
+summary(log_mod_f)
 
 
 
+
+
+
+
+
+
+# Junk ------------------------------------
 
 rl_species <- rl_sp$assessments
 
